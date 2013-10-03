45987a5d57912283a60d3bf8df011515a502eac6
Thomz <kamelenvandrer@hotmail.com>
2013-10-03 15:10:03 +0200

    ImageFiltering.hpp made plus minor funcs

--- main.cpp
+++ main.cpp
@@ -1,120 +1,58 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include "queue"
-using namespace cv;
-using namespace std;
+#include "ImageFiltering.hpp"
 
-template <typename T>
-  string NumberToString ( T Number )
-  {
-     ostringstream ss;
-     ss << Number;
-     return ss.str();
-	//mfskf
-  }
+string getInputObject(){
+	string inputObject;
 
-vector<Mat> filterSurrounding(Mat& input, Mat& output){
-	Mat gray;
-	cvtColor(input, gray, CV_RGB2GRAY);
-	Mat_<float> floatImg = gray.clone();
+	cout << "Write name of wanted object:" << endl;
 
-	Mat_<uchar> smooth = floatImg.clone();
-	Mat_<uchar> edges;
-	Mat edges2;
-	Canny(smooth,edges,50,200,3,1);
-	//imshow("edges",edges);
-	dilate(edges,edges,Mat(),Point(-1,-1),2);
-	//imshow("dilated",edges);
-	//erode(edges,edges,Mat(),Point(-1,-1),2);
-	//imshow("eroded",edges);
+	cin >> inputObject;
 
+	cout << "Looking for " + inputObject << endl;
 
-	copyMakeBorder(edges,edges,2,2,2,2,BORDER_CONSTANT,0); // makes frame of black pixels
-
-	Mat mask;
-	Canny(edges, mask, 100, 200);
-	copyMakeBorder(mask, mask, 1, 1, 1, 1, BORDER_REPLICATE);
-	Mat_<uchar> temp;
-	int paintedWhite = 0;
-	for(int x=0; x<edges.cols;x+=5)		// fills all encapsulated areas with white
-		for(int y=0; y<edges.rows;y+=5){
-			temp = edges.clone();
-			paintedWhite = floodFill(temp,mask,Point(x,y),255);
-			if(paintedWhite*10 < 4*temp.cols*temp.rows){
-				edges=temp;
-			}
-		}
-
-	dilate(edges,edges,Mat(),Point(-1,-1),2);	// expand to close holes and make full bodies
-	erode(edges,edges,Mat(),Point(-1,-1),8);	// erodes to small edges
-	dilate(edges,edges,Mat(),Point(-1,-1),4);	// expands to maintain object size
-
-	Mat blobs = edges.clone();
-	//imshow("filtered", blobs);
-
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	findContours( blobs, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
-
-	/// Draw contours
-	Mat drawing = Mat::zeros( edges.size(), CV_8UC3 );
-	int idx = 0;
-	vector<Mat> singleImgs;
-
-	for( ; idx >= 0; idx = hierarchy[idx][0] )
-	{
-		Scalar color( 255, 255, 255 );
-		drawContours( drawing, contours, idx, color, CV_FILLED, 8, hierarchy );
-		dilate(drawing,drawing,Mat(),Point(-1,-1),5);
-		if(contours[idx].size()>40)
-			singleImgs.push_back(drawing.clone());
-		drawing = Mat::zeros( edges.size(), CV_8UC3 );
-	}
-
-
-	return singleImgs;
-
+	return inputObject;
 }
 
-void getOverlay(Mat& orig, Mat& filtered){
-	for(int x=0; x<filtered.cols;x++)
-		for(int y=0; y<filtered.rows;y++){
-			if(filtered.at<Vec3b>(y,x)[0] == 255)
-			{
-				filtered.at<cv::Vec3b>(y,x)[0] = orig.at<cv::Vec3b>(y,x)[0];
-				filtered.at<cv::Vec3b>(y,x)[1] = orig.at<cv::Vec3b>(y,x)[1];
-				filtered.at<cv::Vec3b>(y,x)[2] = orig.at<cv::Vec3b>(y,x)[2];
-			}
-		}
+Vector<KeyPoint> findObjectInDatabase(string inputObject){
+
+	vector<KeyPoint> result;
+
+	FileStorage fs("test.yml", FileStorage::READ);
+	if (fs.isOpened() == 0){
+		cout << "Object not found in database" << endl;
+		return result;
+	}
+	FileNode kptFileNode = fs["keypoints"];
+	read( kptFileNode, result);
+	fs.release();
+	cout << result.size() << endl;
+
+	return result;
+	;
 }
 
 int main(int argc, char **argv) {
 
-	Mat src = imread("1.png", CV_LOAD_IMAGE_UNCHANGED);
-	imshow("dfs", src);
-	Mat filtered;
+	string inputObject = getInputObject();
+	vector<KeyPoint> keypointsObject  = findObjectInDatabase(inputObject);
 
+
+	Mat src = getImage();
+
+	Mat filtered;
 	vector<Mat> singleObjects;
 	singleObjects = filterSurrounding(src, filtered);
+	vector<KeyPoint> keypoints;
+
+	Mat output;
 
 	for(int i=0; i<singleObjects.size(); i++){
 		getOverlay(src, singleObjects[i]);
+		keypoints = getKeypointsFromObject(singleObjects[i]);
+		drawKeypoints(singleObjects[i], keypoints, singleObjects[i]);
 		imshow(NumberToString(i), singleObjects[i]);
-	}
-
-
-	SiftFeatureDetector detector;
-	Mat output;
-	for(int i=0; i<singleObjects.size(); i++){
-		// Find keypoints in the picture
-		vector<KeyPoint> anc_keypoints;
-		detector.detect(singleObjects[i], anc_keypoints);
-
-		// Draw keypoints on new pic
-
-		drawKeypoints(singleObjects[i], anc_keypoints, output);
-		imshow(NumberToString(10+i), output);
-		output = Mat::zeros( output.size(), CV_8UC3 );
 	}
 
 
