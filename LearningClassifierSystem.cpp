3dab5eed756a59eebd78a18876c2541103d908a3
Thomz <kamelenvandrer@hotmail.com>
2013-10-10 15:35:26 +0200

    Ga working without learning

--- LearningClassifierSystem.cpp
+++ LearningClassifierSystem.cpp
@@ -13,23 +13,31 @@ LearningClassifierSystem::LearningClassifierSystem(){
 LearningClassifierSystem::~LearningClassifierSystem(){
 }
 
-void LearningClassifierSystem::Test(){
-	cout << "Test"<< endl;
-}
-
-void LearningClassifierSystem::learn(Mat descriptorsDatabase, vector<Mat> descriptorsObjects, vector<Mat> singleObjects, string inputString){
+void LearningClassifierSystem::learn(vector<Mat> descriptorsObjects, vector<Mat> singleObjects, string inputString){
 	bool found = false;
+	int GAno;
 	  // Search already known GAs to find desired object
 	for(int i = 0; i < geneticAlgorithms.size(); i++){
 		if(geneticAlgorithms[i].type == inputString){
 			found = true;
+			GAno = i;
 		}
 	}
-
+	int* votes;
+	int highscore(0), highscoreObj(0);
 	if( found ){
 		// Do some AI stuff
 		cout << "Object already in database" << endl;
-		int i = 0;
+		votes = voteForObject(descriptorsObjects,  GAno);
+		for(int b = 0; b < sizeof(votes)/sizeof(*votes); b++){
+			cout << votes[b] << endl;
+			if(votes[b] > highscore){
+				highscore = votes[b];
+				highscoreObj = b;
+			}
+		}
+
+		imshow("highscore", singleObjects[highscoreObj]);
 	}
 	else{
 		cout << "Object not found in database" << endl;
@@ -40,11 +48,54 @@ void LearningClassifierSystem::learn(Mat descriptorsDatabase, vector<Mat> descri
 
 }
 
-void saveObjectToDatabase(string objectName, Mat descriptor){
-	FileStorage fs("database/" + objectName + ".yml", FileStorage::WRITE);
-	write( fs, "Descriptors", descriptor);
-	fs.release();
+int* LearningClassifierSystem::voteForObject(vector<Mat> descriptorsObjects,  int gaNo){
+	int votes[descriptorsObjects.size()];
+	int tempVotes[descriptorsObjects.size()];
+
+	for(int i = 0; i < sizeof(votes)/sizeof(*votes); i++)
+		votes[i] = 0;
+
+//	for(int i = 0; i < sizeof(votes)/sizeof(*votes); i++)
+//		cout << votes[i] << endl;
+
+	FlannBasedMatcher matcher;
+	std::vector< DMatch > matches;
+
+
+		// For every chromosome(set of features) with the wanted type
+	for(int i = 0; i < geneticAlgorithms[gaNo].chromosomes.size(); i++){
+
+
+			// Clear tempVotes
+		for(int j = 0; j < sizeof(tempVotes)/sizeof(*tempVotes); j++)
+			tempVotes[j] = 0;
+
+		// For every object that is in the picture
+		for(int j = 0; j < descriptorsObjects.size(); j++){
+
+				// Match current object with current chromosome
+			matcher.match( geneticAlgorithms[gaNo].chromosomes[i].features, descriptorsObjects[j], matches );
+
+				// Calculate most fittes object
+			for(int h = 0; h < matches.size(); h++)
+				if(matches[h].distance < matchesThreshold)
+					tempVotes[j]++;
+		}
+		int highscore(0), highscoreObj(0);
+
+		for(int b = 0; b < sizeof(votes)/sizeof(*votes); b++){
+			if(tempVotes[b] > highscore){
+				highscore = tempVotes[b];
+				highscoreObj = b;
+			}
+		}
+		votes[highscoreObj]++;
+	}
+
+	return votes;
+
 }
+
 
 void LearningClassifierSystem::findFirstInstance( vector<Mat> descriptorsObjects, vector<Mat> singleObjects, string inputString){
 	string userInpt;
@@ -57,10 +108,11 @@ void LearningClassifierSystem::findFirstInstance( vector<Mat> descriptorsObjects
 		cout << "Please enter if this is " << inputString << endl;
 		cin >> userInpt;
 		if(userInpt == "yes" ||userInpt == "Yes" || userInpt == "y" ){
-			Chromosome tempChr = {descriptorsObjects[i],0};
+			Chromosome tempChr = {descriptorsObjects[i].clone(),0};
 			GA tempGA;
 			tempGA.type = inputString;
 			tempGA.chromosomes.push_back(tempChr);
+			geneticAlgorithms.push_back(tempGA);
 			break;
 		}
 	}
